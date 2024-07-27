ea6885ebf7e2cb31419765034fa666f142dbf7b1
Passerby <1300547758@qq.com>
2024-07-27 09:26:13 +0800

    1.新建了order-card，因为可能用户和订单页面都要用同个组件 2.初步美化了UserCenter

---
+++ src/components/OrderCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <span>订单号{{ props.orderId }}</span>
+        <span>电影名{{ props.movieName }}</span>
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        orderId: Number,
+        movieName: String,
+        cinemaName: String,
+        hallNumber: String,
+        seatNumber: String,
+        ticketCount: Number,
+        orderPrice: Number,
+        showTime: String,
+        orderTime: String,
+        ticketPrice: Number
+    })
+</script>
+
+<style scoped>
+    .container {
+        border-radius: 10px;
+        box-shadow: 0px 3px 10px 3px black;
+    }
+</style>
--- src/components/UserCenter.vue
+++ src/components/UserCenter.vue
@@ -1,18 +1,39 @@
 <template>
     <movie-header-backup></movie-header-backup>
-    <div id="content-area">
-        <div id="avatar">{{ user.first_letter }}</div>
-        <div id="username">{{ user.name }}</div>
-        <div id="gender">{{ user.gender }}</div>
-        <div id="age">{{ user.age }}</div>
-        <div @click="logout()">登出</div>
+    <div id="intro-area">
+        <div id="intro-left">
+            <div id="avatar">{{ user.first_letter }}</div>
+            <div @click="logout()" id="logout-btn">登出</div>
+        </div>
+            <div id="intro-right">
+                <div class="intro-block">
+                <span>用户名</span>
+                <span id="username">{{ user.name }}</span>
+            </div>
+            <div class="intro-block">
+                <span>性别</span>
+                <span id="gender">{{ user.gender }}</span>
+            </div>
+            <div class="intro-block">
+                <span>年龄</span>
+                <span id="age">{{ user.age }}</span>
+            </div>
+        </div>
+
     </div>
+    <div id="order-area">
+        <span>订单</span>
+        <div v-for="order in orders.arr">
+            <order-card v-bind="order"></order-card>
+        </div>
+    </div>  
     <!-- <span id="username">fd</span> -->
     <!-- <span>d</span> -->
 </template>
 
 <script setup>
     import MovieHeaderBackup from './MovieHeaderBackup.vue';
+    import OrderCard from './OrderCard.vue';
     import { reactive } from 'vue';
     import axios from 'axios';
     import router from '..';
@@ -25,6 +46,10 @@
         first_letter: ''
     });
 
+    const orders = reactive({
+        arr: []
+    });
+
     async function fetchUser() {
         try {
             const response = await axios.post("/user/getUserInfo");
@@ -33,13 +58,29 @@
                 user.id = data.userId;
                 user.name = data.userName;
                 user.age = data.age;
-                user.gender = data.gender;
+                if (data.gender == 'M') {
+                    user.gender = '男';
+                } else {
+                    user.gender = '女';
+                }
                 user.first_letter = user.name[0].toUpperCase();
+                fetchOrder();
             } else {
                 // 判定为没有登录，转到登录界面
                 router.push("/login");
             }
         } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function fetchOrder() {
+        try {
+            const response = await axios.get(`/orders/${user.id}`);
+            if (response.data.code == 200) {
+                orders.arr = response.data.data;
+            }
+        } catch(error) {
             console.log(error);
         }
     }
@@ -61,10 +102,15 @@
 </script>
 
 <style scoped>
-    #content-area {
+    #intro-area {
         display: flex;
         width: 70%;
         margin: 100px auto;
+        justify-content: center;
+    }
+
+    #intro-left {
+        width: 40%;
     }
 
     #avatar {
@@ -78,5 +124,30 @@
         align-items: center;
         border-radius: 40px;
         font-weight: bold;
+    }
+
+    #username {
+        font-size: 2em;
+    }
+
+    .intro-block {
+        width: 100%;
+    }
+
+    .intro-block span {
+        margin-right: 30px;
+    }
+
+    #logout-btn {
+        width: fit-content;
+    }
+
+    #logout-btn:hover {
+        cursor: pointer;
+    }
+
+    #order-area {
+        width: 70%;
+        margin: 10vh auto;
     }
 </style>
